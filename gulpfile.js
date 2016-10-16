d442a46038a307723fdf72c84479629a24696c84
samblackmore <samblackmore07@gmail.com>
2016-10-16 18:14:48 -0400

    piping data into pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 
 gulp.task('default', function() {
+
+  var locals = {
+    title: 'Hello'
+  }
+
   return gulp.src('index.pug')
-  .pipe(pug())
+  .pipe(pug({locals: locals}))
   .pipe(gulp.dest('./'))
 });
