b5872d78252a9306e360e9a769760194bd4e96d5
samblackmore <samblackmore07@gmail.com>
2016-10-17 18:37:49 -0400

    moving config to json file

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,9 @@
 var gulp = require('gulp'),
     pug = require('gulp-pug'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch')
+    locals = require('./config.json');
 
 gulp.task('default', function() {
-  var locals = {
-    title: 'Hello',
-    panels: [{
-      name: '3 Words',
-      src: 'img/words.png',
-      desc: 'Write a story 3 words at a time'
-    }, {
-      name: 'Get to the Bottom!',
-      src: 'img/bottom.png',
-      desc: 'Turn-based Pacman'
-    }]
-  }
-
   return watch('index.pug')
     .pipe(pug({locals: locals}))
     .pipe(gulp.dest('./'));
