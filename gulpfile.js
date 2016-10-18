fc8a5f56479a9f73c5d5403e52dd0b26b739cbbe
samblackmore <samblackmore07@gmail.com>
2016-10-17 20:02:00 -0400

    improving gulp watch
    gulpfile build then watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
+var watches = ['*.pug', '*.json'];
+
 var gulp = require('gulp'),
     pug = require('gulp-pug'),
-    watch = require('gulp-watch')
-    locals = require('./config.json');
+    watch = require('gulp-watch'),
+    gutil = require('gulp-util');
 
 gulp.task('default', function() {
-  return watch('index.pug')
-    .pipe(pug({locals: locals}))
-    .pipe(gulp.dest('./'));
+	gutil.log('Building index.html');
+	build();
+	gutil.log('Watching for changes...');
+ 	return watch(watches, build);
 });
+
+function build() {
+	var locals = require('./config.json');
+
+	gulp.src('index.pug')
+	.pipe(pug({locals: locals}))
+	.pipe(gulp.dest('./'))
+	.on('end', function(){ gutil.log('index.html rebuilt!')})
+}
